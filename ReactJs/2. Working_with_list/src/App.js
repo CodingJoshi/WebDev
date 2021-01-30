e1c7e1bd0c29748c81823d0b2120de7203bc6a91
CodingJoshi <joshisaurabh2610@gmail.com>
2021-01-30 12:06:16 +0530

    Outputing Lists

--- ReactJs/2. Working_with_list/src/App.js
+++ ReactJs/2. Working_with_list/src/App.js
@@ -18,7 +18,28 @@ class App extends Component{
     this.setState({
       TogglePerson: !isShown
     })
-  }
+  };
+  DeletePersonHandler=(index,event)=>{
+    // console.log('Person Deleted');
+    // console.log(index);
+    /*
+    const persons=this.state.persons;
+    persons.splice(index,1);
+    this.setState({
+      persons:persons
+    })
+    */
+    // above method is not a good practice
+    
+    // good practice is to make a new copy of array first 
+    // const Persons=this.state.persons.slice();
+    const Persons=[...this.state.persons];
+    Persons.splice(index,1);
+    this.setState({
+      persons:Persons
+    })
+
+  };
   render(){
     const style={
       backgroundColor: 'black',
@@ -33,16 +54,11 @@ class App extends Component{
       person=(
        <div className="Persons">
           {
-            this.state.persons.map(person=>{
-              return <Person name={person.name} age={person.age} click={DeletePersons} />
+            this.state.persons.map((person,index)=>{
+              return <Person name={person.name} age={person.age} click={()=>this.DeletePersonHandler(index)} />
             })
          }
        </div>
-        // <div className="Persons">
-        //   <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        //   <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        //   <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        // </div>
       );
     }
     return (
@@ -54,90 +70,4 @@ class App extends Component{
     );
   }
 }
-// Method-2 Conditional 
-/*
-class App extends Component{
-  state={
-    person:[
-      {name:'Saurabh Joshi', age:20},
-      {name:'Vicky' , age: 12},
-      {name:'Payal', age: 16},
-    ],
-    TogglePerson:false
-  }
-  ToggleHandler=()=>{
-    const isShown=this.state.TogglePerson;
-    this.setState({
-      TogglePerson: !isShown
-    })
-  }
-  render(){
-    const style={
-      backgroundColor: 'black',
-      border: '2px solid blue',
-      borderRadius: '20%',
-      padding:'10px',
-      color: 'white',
-      outline:'none',
-    }
-    let person=null;
-    if(this.state.TogglePerson){
-      person=(
-        <div className="Persons">
-          <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-          <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-          <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-        </div>
-      );
-    }
-    return (
-      <div className="App">
-        <h1>Persons</h1>
-        <button onClick={this.ToggleHandler} style={style}>Toggle Persons</button>
-        {person}
-      </div>
-    );
-  }
-}
-*/
-// Method-1 conditional
-/*
-class App extends Component{
-  state={
-    TogglePerson:false
-  }
-  ToggleHandler=()=>{
-    const isShown=this.state.TogglePerson;
-    this.setState({
-      TogglePerson: !isShown
-    })
-  }
-  render(){
-    const style={
-      backgroundColor: 'black',
-      border: '2px solid blue',
-      borderRadius: '20%',
-      padding:'10px',
-      color: 'white',
-      outline:'none',
-    }
-    return (
-      <div className="App">
-        <h1>Persons</h1>
-        <button onClick={this.ToggleHandler} style={style}>Toggle Persons</button>
-        {
-          this.state.TogglePerson ?
-            <div className="Persons">
-              <Person name="Saurabh" age={20}/>
-              <Person name="Vicky" age={12}/>
-              <Person name="Payal" age={16}/>
-            </div>
-          :null
-        }
-      </div>
-    );
-  }
-}
-*/
-
 export default App;
