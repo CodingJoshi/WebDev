e9f2d61e2c898f3441cb51b8bbf9b3f79bcf61f8
CodingJoshi <joshisaurabh2610@gmail.com>
2021-02-08 22:41:09 +0530

    React.Fragment is same as Aux

--- ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
+++ ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
@@ -5,13 +5,15 @@ class Person extends Component{
     render(){
         console.log('[Person.js] rendering...')
         return (
-            <Aux>
+            <React.Fragment>
                 <p>I am <i><b className="red">{this.props.name}</b></i></p>
                 <p><b>Age: </b>{this.props.age}</p>
                 <p><b>Address: <br/> </b>{this.props.Add}</p>
                 <input onChange={this.props.inputChange} value={this.props.name}></input>
-            </Aux>
+            </React.Fragment> 
         )
     }
 }
 export default Person;
+
+// React.Fragment is inbuilt Aux works same as Aux works
