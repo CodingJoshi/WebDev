47b18643e1b3261bdccd5068368d4745f9a6a7d4
CodingJoshi <joshisaurabh2610@gmail.com>
2021-02-08 22:38:31 +0530

    Rendering adjacent JSX by using Aux

--- ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
+++ ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
@@ -1,16 +1,16 @@
 import React,{Component} from 'react'
 import classes from './Person.module.css'
-
+import Aux from '../../../hoc/Auxillary'
 class Person extends Component{
     render(){
         console.log('[Person.js] rendering...')
         return (
-            [
-                <p>I am <i><b className="red">{this.props.name}</b></i></p>,
-                <p><b>Age: </b>{this.props.age}</p>,
-                <p><b>Address: <br/> </b>{this.props.Add}</p>,
-                <input onChange={this.props.inputChange} value={this.props.name}></input>,
-            ]
+            <Aux>
+                <p>I am <i><b className="red">{this.props.name}</b></i></p>
+                <p><b>Age: </b>{this.props.age}</p>
+                <p><b>Address: <br/> </b>{this.props.Add}</p>
+                <input onChange={this.props.inputChange} value={this.props.name}></input>
+            </Aux>
         )
     }
 }
