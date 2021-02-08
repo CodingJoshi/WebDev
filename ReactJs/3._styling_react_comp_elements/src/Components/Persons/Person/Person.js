1f6b56b244790f3a0e1e8880be06c44165aa5f89
CodingJoshi <joshisaurabh2610@gmail.com>
2021-02-08 22:35:40 +0530

    Rendering adjacent  JSX elements by returning an  array

--- ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
+++ ReactJs/3._styling_react_comp_elements/src/Components/Persons/Person/Person.js
@@ -5,12 +5,12 @@ class Person extends Component{
     render(){
         console.log('[Person.js] rendering...')
         return (
-            <div className={classes.Person} onClick={this.props.Delete}>
-                <p>I am <i><b className="red">{this.props.name}</b></i></p>
-                <p><b>Age: </b>{this.props.age}</p>
-                <p><b>Address: <br/> </b>{this.props.Add}</p>
-                <input onChange={this.props.inputChange} value={this.props.name}></input>
-            </div>
+            [
+                <p>I am <i><b className="red">{this.props.name}</b></i></p>,
+                <p><b>Age: </b>{this.props.age}</p>,
+                <p><b>Address: <br/> </b>{this.props.Add}</p>,
+                <input onChange={this.props.inputChange} value={this.props.name}></input>,
+            ]
         )
     }
 }
